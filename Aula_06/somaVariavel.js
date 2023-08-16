c5fe3e4df85aab39cb2ea2c3720fb4b25b09020d
Lucas Augusto <77333486+Lucassilva1010@users.noreply.github.com>
2023-08-15 23:25:38 -0300

    Exercicios da aula 06

---
+++ Aula_06/somaVariavel.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+let x = 3
+let y = 35
+let z = 9
+
+let resultado;
+
+resultaddo = ((x+y)>90) && (z != 70)
+resultado = ((x+y) >90 || (x+y) == 90) && (z!= 70) 
+
+
